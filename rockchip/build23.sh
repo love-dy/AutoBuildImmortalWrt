dfb2eba88db759994221e078a53dc88bf661c6ea
飞翔的荷兰人 <ilove11502@gmail.com>
2025-12-30 12:03:45 +0800

    Update build23.sh

--- rockchip/build23.sh
+++ rockchip/build23.sh
@@ -51,14 +51,8 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 # 定义所需安装的包列表 23.05.4 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
-PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 代理工具
@@ -70,11 +64,68 @@ if [ "$INCLUDE_DOCKER" = "yes" ]; then
     echo "Adding package: luci-i18n-dockerman-zh-cn"
 fi
 # 静态文件服务器dufs(推荐)
-PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
+# 增加***
+PACKAGES="$PACKAGES kmod-usb-core"
+PACKAGES="$PACKAGES kmod-usb-net"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-eem"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-ether"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-mbim"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-ncm"
+PACKAGES="$PACKAGES kmod-usb-net-cdc-subset"
+PACKAGES="$PACKAGES kmod-usb-net-huawei-cdc-ncm"
+PACKAGES="$PACKAGES kmod-usb-net-ipheth"
+PACKAGES="$PACKAGES kmod-usb-net-qmi-wwan"
+PACKAGES="$PACKAGES kmod-usb-net-rndis"
+PACKAGES="$PACKAGES kmod-usb-net-rtl8150"
+PACKAGES="$PACKAGES kmod-usb-net-rtl8152-vendor"
+PACKAGES="$PACKAGES kmod-usb-ehci"
+PACKAGES="$PACKAGES kmod-usb-ohci"
+PACKAGES="$PACKAGES kmod-usb-uhci"
+PACKAGES="$PACKAGES kmod-usb2"
+PACKAGES="$PACKAGES kmod-usb3"
+PACKAGES="$PACKAGES kmod-usb-storage"
+PACKAGES="$PACKAGES kmod-usb-storage-extras"
+PACKAGES="$PACKAGES kmod-usb-storage-uas"
+# 增加***
+PACKAGES="$PACKAGES cfdisk"
+PACKAGES="$PACKAGES wget-ssl"
+PACKAGES="$PACKAGES ip6tables-extra"
+PACKAGES="$PACKAGES ip6tables-mod-nat"
+PACKAGES="$PACKAGES iptables-nft"
+PACKAGES="$PACKAGES ip6tables-nft"
+# PACKAGES="$PACKAGES qrencode"
+PACKAGES="$PACKAGES kmod-mtd-rw"
+# 增加***
+PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-smartdns-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"
+# PACKAGES="$PACKAGES luci-proto-wireguard"
+# PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+# 增加***
+PACKAGES="$PACKAGES luci-theme-bootstrap"
+PACKAGES="$PACKAGES luci-theme-argon"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-theme-material"
+# 增加***
+PACKAGES="$PACKAGES kmod-drm-panfrost"
+PACKAGES="$PACKAGES kmod-drm-rockchip"
+PACKAGES="$PACKAGES luci-i18n-cpufreq-zh-cn"
+# 增加***
+PACKAGES="$PACKAGES wpad-openssl"
+PACKAGES="$PACKAGES kmod-mt7921e"
+PACKAGES="$PACKAGES kmod-mt7921-firmware"
 
 # 若构建openclash 则添加内核
 if echo "$PACKAGES" | grep -q "luci-app-openclash"; then
