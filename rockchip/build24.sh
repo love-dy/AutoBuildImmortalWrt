8c4c9bae941a1829e48bc3f7e013872959b8e19b
飞翔的荷兰人 <ilove11502@gmail.com>
2026-03-02 14:52:00 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -70,7 +70,7 @@ if [ "$INCLUDE_DOCKER" = "yes" ]; then
     echo "Adding package: luci-i18n-dockerman-zh-cn"
 fi
 # 文件管理器
-# PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
@@ -132,10 +132,6 @@ PACKAGES="$PACKAGES luci-i18n-cpufreq-zh-cn"
 PACKAGES="$PACKAGES wpad-openssl"
 PACKAGES="$PACKAGES kmod-mt7921e"
 PACKAGES="$PACKAGES kmod-mt7921-firmware"
-
-# ======== shell/custom-packages.sh =======
-# 合并imm仓库以外的第三方插件
-# PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
